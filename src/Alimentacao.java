7d180e7cca71e9383732439d44c1a75a86138cfd
Ramiro Manoel <ramiro.manoel77@gmail.com>
2023-09-24 15:33:10 -0300

    correção de metodosna classe shopping

--- src/Alimentacao.java
+++ src/Alimentacao.java
@@ -3,6 +3,8 @@ public class Alimentacao extends Loja {
 	
 	private Data dataAlvara;
 
+	
+	
 	public Alimentacao(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario, Endereco endereco,
 			Data dataFundacao, Data dataAlvara, int tamanho) {
 		super(nome, quantidadeFuncionarios, salarioBaseFuncionario, endereco, dataFundacao, tamanho);
